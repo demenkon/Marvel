036e06225ac144fdbe6b750627de87306a73065c
Nikita <demenkonikita@gmail.com>
2022-04-24 15:28:11 +0300

    lesson 187

--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -3,15 +3,15 @@ import { useParams, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 import useMarvelService from '../../services/MarvelService';
-import Spinner from '../spinner/Spinner';
-import ErrorMessage from '../errorMessage/ErrorMessage';
 import AppBanner from "../appBanner/AppBanner";
+import setContent from '../../utils/setContent';
 import './singleComicPage.scss';
+import { Component } from 'react/cjs/react.production.min';
 
 const SingleComicPage = () => {
    const { comicId } = useParams();
    const [comic, setComic] = useState(null);
-   const { loading, error, getComic, clearError } = useMarvelService();
+   const { getComic, clearError, process, setProcess } = useMarvelService();
 
    useEffect(() => {
       updateComic()
@@ -21,28 +21,23 @@ const SingleComicPage = () => {
       clearError();
       getComic(comicId)
          .then(onComicLoaded)
+         .then(() => setProcess('confirmed'))
    }
 
-   const onComicLoaded = (comic) => {
-      setComic(comic);
+   const onComicLoaded = (data) => {
+      setComic(data);
    }
-
-   const errorMessage = error ? <ErrorMessage /> : null;
-   const spinner = loading ? <Spinner /> : null;
-   const content = !(loading || error || !comic) ? <View comic={comic} /> : null;
 
    return (
       <>
          <AppBanner />
-         {errorMessage}
-         {spinner}
-         {content}
+         {setContent(process, View, comic)}
       </>
    )
 }
 
-const View = ({ comic }) => {
-   const { title, description, pageCount, thumbnail, language, price } = comic;
+const View = ({ data }) => {
+   const { title, description, pageCount, thumbnail, language, price } = data;
 
    return (
       <div className="single-comic">
--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,15 +1,17 @@
-import {useState, useCallback} from 'react';
+import { useState, useCallback } from 'react';
 
 export const useHttp = () => {
    const [loading, setLoading] = useState(false);
    const [error, setError] = useState(null);
+   const [process, setProcess] = useState('waiting');
 
-   const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'applycation/json'}) => {
+   const request = useCallback(async (url, method = 'GET', body = null, headers = { 'Content-Type': 'applycation/json' }) => {
 
       setLoading(true);
+      setProcess('loading');
 
-      try{
-         const response = await fetch(url, {method, body, headers});
+      try {
+         const response = await fetch(url, { method, body, headers });
 
          if (!response.ok) {
             throw new Error(`Could not fetch ${url}, status: ${response.status}`);
@@ -19,15 +21,19 @@ export const useHttp = () => {
 
          setLoading(false);
          return data;
-      } catch(e) {
+      } catch (e) {
          setLoading(false);
          setError(e.message);
+         setProcess('error');
          throw e;
       }
-      
-    }, []);
 
-    const clearError = useCallback(() => setError(null), []);
+   }, []);
 
-    return {loading, request, error, clearError}
+   const clearError = useCallback(() => {
+      setError(null);
+      setProcess('loading');
+   }, []);
+
+   return { loading, request, error, clearError, process, setProcess }
 }
